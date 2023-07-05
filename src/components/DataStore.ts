43b4fff5511f1a75c3bb3901775c8e9a2ff27af8
marc bulbulian <boolioncube@gmail.com>
2023-07-05 00:16:49 -0400

    dataStore
    - streamElements API
    - try browser storage for JSON

--- src/components/DataStore.ts
+++ src/components/DataStore.ts
@@ -1,12 +1,33 @@
+const elementsAPI = 'https://api.streamelements.com/kappa/v2/store/61e8d63d3d12f65a5584b351/items?source=website'
+const elementsUser = (u:string) => `https://api.streamelements.com/kappa/v2/points/61e8d63d3d12f65a5584b351/${u}`
+const JSONheader = { headers: { accept: 'application/json, text/plain, */*'}}
 
 const getters = { 
-  getJSON: async (jsonFile:string) => 
-    fetch(`/${jsonFile}.json`)
-      .then(r=>r.json())
-      .then(r=>{ 
-        dataStorage[jsonFile] = r
-        return r 
+  savedData: (k:string) => { // init browserData
+    try { 
+      const t = localStorage.getItem(k)
+      const j = t?.length ? JSON.parse(t) : []
+      dataStorage[`${k}Games`] = j
+      console.log('List loaded', k, j )
+    } catch(e) {
+      console.log('List INIT', k, e)
+      dataStorage[`${k}Games`] = []
+    }
+  },
+  elements: { 
+    ls: () => fetch(elementsAPI, JSONheader)
+      .then( r => r.json() )
+      .then( games => { 
+        dataStorage.games = games
+        return games 
       }),
+    user: (u:string) => fetch( elementsUser(u), JSONheader)
+      .then( r => r.json() )
+      .then( d => { 
+        dataStorage.user = d
+        return d
+      })
+  }
 }
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const dataStorage: Record<string,any> = reactive({
