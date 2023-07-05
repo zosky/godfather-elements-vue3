61f7203d29cbaabf4ec81f41734a43aa7e94bd4e
marc bulbulian <boolioncube@gmail.com>
2023-07-05 00:07:17 -0400

    haveGames page

---
+++ src/pages/have.vue
@@ -0,0 +1,23 @@
+<route lang="json">
+  {"meta":{"title":"have" }}
+</route>
+<script setup>
+const dataStore = inject('$dataStore')
+const games = computed(()=>{
+  const s = dataStore?.sort ?? 'cost'
+  const h = dataStore?.haveGames ?? []
+  return dataStore?.games
+    ?.filter(g=> h.includes(g._id) ) // only ignored
+    ?.sort((a,b)=> 
+      s=='cost' ? a.cost>b.cost? 1 :-1  
+        : s=='date' ? a.updatedAt > b.updatedAt ? 1: -1 
+          : s=='name' ? a.name > b.name ? 1 : -1 
+            : true)
+})
+</script>
+
+<template>
+  <div class="grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-2 p-2">
+    <GameCard v-for="game of games" :key="game._id" :game="game" />
+  </div>
+</template>
