d782c396f64565ff22c72ee50877fd6238d050f1
marc bulbulian <boolioncube@gmail.com>
2023-08-26 01:16:11 -0400

    gaStats w/ curUser

--- src/components/leaderboard/GAstats.vue
+++ src/components/leaderboard/GAstats.vue
@@ -13,10 +13,23 @@ const GAdata = computed(()=>{
   if (liveDataOn.value) h.push(...liveArr.value)
   return h
 })
+const myWins = computed(()=>{
+  const h = []
+  const me = dataStore?.user?.username
+  if (me){
+    if (cacheDataOn.value) h.push( ... Object.values( gaHistory.value?.[me]??{}) )
+    if (liveDataOn.value) h.push( ... Object.values(liveLog.value?.[me]??{}) )
+  }
+  return h?.reduce((a,c)=>{ if(c) { !a?.[c] ? a[c]=1 : a[c]++ } ; return a},{})
+})
 const gaStats = computed(()=>{
   const h = GAdata.value
   const GAuniq = Array.from(new Set(h)).sort((a,b)=>a<b?-1:1)
-  const GAcounts = GAuniq.map(clams => clams={clams,count: h.filter(hh=>hh==clams).length})
+  const GAcounts = GAuniq.map(clams => clams={
+    clams,
+    count: h.filter(hh=>hh==clams).length,
+    mine: myWins.value?.[clams]
+  })
   return GAcounts
 })
 watchEffect(()=>{ const d = cacheDataOn.value ; dataStore.cacheDataOn = d })
@@ -26,7 +39,7 @@ watchEffect(()=>{ const d = liveDataOn.value ; liveDataOn.cacheDataOn = d })
 <template>
   <div id="gaStats">
     <div class="flex flex-row w-full items-center gap-3 pb-1">
-      <div class="border-b border-purple-700">
+      <div>
         <b v-text="GAdata?.length" /> giveAways 
       </div> 
       <div class="flex flex-row gap-1 text-sm select-none">
@@ -48,8 +61,15 @@ watchEffect(()=>{ const d = liveDataOn.value ; liveDataOn.cacheDataOn = d })
         </label>
       </div>
     </div>
-    <div class="grid grid-cols-3 gap-x-3 text-right max-w-max">
+    <div class="grid grid-cols-4 gap-x-3 text-right max-w-max">
       <template v-for="(ga,gx) of gaStats" :key="gx">
+        <div class="flex flex-row items-center justify-end w-full bg-purple-950 bg-opacity-40 px-2" >
+          <h6 v-if="ga?.mine" :title="`wining ${ga.clams} x ${ga?.mine} times = ${ga.clams * ga.mine}`" class="font-mono flex flex-row justify-end gap-0.5 text-xs  py-1">
+            <div class="number" v-text="ga?.mine" />
+            🙋
+            <div class="percent" v-text="((ga?.mine/ga?.count*10000)/100).toFixed(3)" />
+          </h6>
+        </div>
         <div class="number font-light" v-text="ga.count" />
         <Clams :clams="ga.clams" :class="dataStore?.konami ? 'money': 'clams'" />
         <b class="percent font-mono" v-text="(Math.round(ga.count/GAdata?.length*10000)/100).toFixed(2)" />
