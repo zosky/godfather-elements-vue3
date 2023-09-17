6c8413c692df79d25b58bdd2174288ee48c87d34
marc bulbulian <boolioncube@gmail.com>
2023-09-17 18:42:07 -0400

    clams value updated
    per convo w/ TGF

--- src/components/Clams.vue
+++ src/components/Clams.vue
@@ -2,14 +2,14 @@
 const props = defineProps({clams:{type:Number, default:0}})
 const dataStore = inject('$dataStore')
 const showAsMoney = computed(()=> dataStore?.konami)
-const money = computed(()=> (props.clams/100)*2 )
+const money = computed(()=> (props.clams/100) )
 const vals = computed(()=>{
   const n = props.clams
   const v = n >= 1000 ? `${(n/1000).toFixed(1)}k` : n
   const m = (money.value/100).toFixed(money.value>=1000?0:2)
   const t = showAsMoney?.value ? m : v
   const d = '\n*but 🐚 have no real world value'
-  const altTxt = showAsMoney?.value ? `${v}🐚 = ~${m}$💸USD${d}` : `${props.clams}🐚`
+  const altTxt = showAsMoney?.value ? `${v}🐚 might be worth ~${m}$💸USD${d}` : `${props.clams}🐚`
   return { clams: v, money: m, this: t, altTxt }
 })
 </script>
