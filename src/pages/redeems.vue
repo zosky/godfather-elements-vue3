4c15471c5134cb2b4fbd9df20ae4b60c5ab5c8aa
marc bulbulian <boolioncube@gmail.com>
2024-01-23 17:07:32 -0500

    redeem details alignment using font-mono

--- src/pages/redeems.vue
+++ src/pages/redeems.vue
@@ -33,6 +33,10 @@ const gameNameHashMap = computed(()=>
     <label class="col-span-full flex w-full justify-end" :title="`${redeems.length} people now have ${totalCount} games\nthanks to TGF`">
       <b class="people" v-text="redeems.length" />
       <b class="games" v-text="totalCount" />
+      <a
+        class="border rounded-xl bg-purple-950 px-2 border-purple-800" href="https://streamelements.com/dashboard/account/redemptions"
+        title="open the steamElements redeems page" target="steamElements"
+        v-text="`my🔑s`"/>
     </label>
     <details v-for="game of redeems" :key="game?.name">
       <summary>
@@ -42,7 +46,7 @@ const gameNameHashMap = computed(()=>
       </summary>
       <ul>
         <li v-for="(user, date) of game?.data" :key="date">
-          <span class="font-light" v-text="moment(date,'X').format('MMM DD HH:mm')" />
+          <span class="font-light font-mono text-xs" v-text="moment(date,'X').format('MMM DD HH:mm')" />
           <b v-text="user" />
         </li>
       </ul>
@@ -56,7 +60,7 @@ const gameNameHashMap = computed(()=>
   summary .count { @apply absolute top-0 right-0 px-2 p-1 rounded-xl bg-opacity-75 bg-purple-900 text-yellow-300 font-bold }
   summary .count::before{ content: '#' ; @apply font-light }
   .count.me::after{ content: '🙋'}
-  ul { @apply p-1 flex flex-col-reverse }
+  ul { @apply p-1 flex flex-col-reverse leading-none }
   b { @apply px-1 }
   .people::after{content:'🧑‍🤝‍🧑'}
   .games::after{content:'👾'}
