5292f939df0613ea5511be4f21120d11d80d24f0
marc bulbulian <boolioncube@gmail.com>
2024-02-26 08:21:50 -0500

    load customParseFormats for dateConverting

--- src/components/leaderboard/GAhistoryUser.vue
+++ src/components/leaderboard/GAhistoryUser.vue
@@ -2,6 +2,8 @@
 import { GiftOutline, CalendarToday } from 'mdue'
 import dayjs from 'dayjs'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+dayjs.extend(customParseFormat)
 dayjs.extend(advancedFormat)
 
 const dataStore = inject('$dataStore')
