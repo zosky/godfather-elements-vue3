410b739b133ee79d3c8cfb3837f4fddf43ac230a
marc bulbulian <boolioncube@gmail.com>
2024-09-16 14:34:50 -0400

    new IA page
    - InventoryAssistant

--- src/pages/inventory.vue
+++ src/pages/inventory.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { CameraOutline, Api, Twitch, Calculator, CheckboxMarkedCircleOutline } from 'mdue'
+import { CameraOutline, Api, Twitch, Calculator, CheckboxMarkedCircleOutline, Eye, EyeOffOutline } from 'mdue'
 import dayjs from 'dayjs'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
 dayjs.extend(advancedFormat)
@@ -10,8 +10,10 @@ const me = computed(()=> dataStore?.user?.username )
 const findSomething = ref('')
 const findUser = ref('')
 const thisGame = ref(null)
-const onlyChanged = ref(true)
-const mismatched = ref(true)
+const onlyEnabled = ref(true)
+const onlyChanged = ref(false)
+const mismatched = ref(false)
+const empty = ref(false)
 const gameNameHashMap = computed(()=> dataStore?.gamesHashMap )
 const perPerson = ref(false)
 cache('inventorySnapshot')
@@ -30,10 +32,11 @@ const snapshot = computed(()=>{
     const gAPI = api?.[g]?.quantity?.current
     const gRed = redeems?.[g]?.filter(gg=>gg.time/1000>game?.time)
     const enabled = api?.[g]?.enabled
-    return { ... game, what: game?.what ?? g, api: gAPI, redeems: gRed, enabled }
+    const cost = api?.[g]?.cost
+    return { ... game, what: game?.what ?? g, api: gAPI, redeems: gRed, enabled, cost }
   })
   return newD
-    ?.filter(g=>g?.enabled)
+    // ?.filter(g=>g?.enabled)
     ?.sort((a,b)=>a?.what>b?.what?1:-1)
   // return d?.map(dd=>dd={
   //   ...dd,
@@ -41,25 +44,42 @@ const snapshot = computed(()=>{
   //   redeems: redeems?.[dd.what]?.filter(g=>g.time/1000>dd.time)
   // })
 })
+const onlyChangedFilter = () => {
+  if (!empty.value) { // will change soon
+    mismatched.value = false 
+    onlyChanged.value = false
+  }
+}
 </script>
 
 <template>
   <section>
     <article class="px-2 max-w-fit m-2 flex flex-row gap-2">
-      <label role="button" :class="{'font-bold':onlyChanged}" class="hover:bg-purple-950 rounded-xl p-2">
-        {{ onlyChanged ? '🖱️ to show all 🎮' : '🖱️ to show 🎮 w/ redeems' }}
+      <label role="button" :class="{on:onlyEnabled}" class="hover:bg-purple-950 rounded-xl p-2">
+        {{ `${onlyEnabled ? 'enabled' : 'all'} ${snapshot.filter(g=>g?.enabled)?.length} (${snapshot.length}) 🎮` }}
+        <input type="checkbox" v-model="onlyEnabled" class="hidden" />
+      </label>
+      <label role="button" :class="{on:onlyChanged}" class="hover:bg-purple-950 rounded-xl p-2">
+        {{ (onlyChanged ? 'only ':'show ') + `${snapshot.filter(g=>g?.qty != g?.api)?.length} 🎮 w/ redeems` }}
         <input type="checkbox" v-model="onlyChanged" class="hidden" />
       </label>
-      <label role="button" :class="{'font-bold':mismatched}" class="hover:bg-purple-950 rounded-xl p-2">
-        {{ mismatched ? 'showing only "misMatched" (🖱️ for all 🎮)' : 'showing all 🎮 (🖱️ for mismatched only)' }}
+      <label role="button" :class="{on:mismatched}" class="hover:bg-purple-950 rounded-xl p-2">
+        {{ `${mismatched ? 'only' : 'show'} ${snapshot.filter(game=>(game?.enabled && (game?.qty - (game?.redeems?.length??0)) != game?.api))?.length} "misMatched" 🎮` }}
         <input type="checkbox" v-model="mismatched" class="hidden" />
       </label>
+      <label role="button" :class="{on:empty}" class="hover:bg-purple-950 rounded-xl p-2" @click="onlyChangedFilter()">
+        {{ snapshot?.filter(g=>!onlyEnabled?true:g?.enabled)?.filter(g=>!g?.api)?.length }} empty
+        <input type="checkbox" v-model="empty" class="hidden" />
+      </label>
+      🖱️ for other stuff
     </article>
     <table>
       <tr>
         <th class="flex flex-row items-start px-1">
           <input type="search" v-model="findSomething" placeholder="🔎game" />
         </th>
+        <th class="num" title="clams"><Eye/></th>
+        <th class="num" title="clams">🐚</th>
         <th class="num" title="snapshot data"><CameraOutline /></th>
         <th class="num" title="redeems announced in chat"><Twitch /></th>
         <th class="num" title="SE API NOW"><Api /></th>
@@ -70,24 +90,31 @@ const snapshot = computed(()=>{
       </tr>
       <tr v-for="game of snapshot?.filter(g=> 
         ( !findSomething ? true : g?.what?.toLowerCase()?.includes( findSomething?.toLowerCase() ) )
-        && (!findUser ? true : g?.redeems?.find(r=>r?.user==findUser)?.time )
+        && (!findUser ? true : g?.redeems?.find(r=>r?.user?.toLowerCase()?.includes( findUser?.toLowerCase() ))?.user )
         && (!onlyChanged ? true : g?.qty != g?.api) 
+        && (!onlyEnabled ? true : g?.enabled ) 
+        && (!empty ? true : !g?.api ) 
         && (!mismatched ? true : (g?.qty - (g?.redeems?.length??0)) != g?.api) )" :key="game?.what" >
         <!-- <td class="date" v-text="dayjs.unix(game.time).format('MMM Do')" /> -->
         <td class="name" v-text="game?.what" />
+        <td class="num">
+          <Eye v-if="game?.enabled" />
+          <EyeOffOutline v-else />
+        </td>
+        <td class="num" v-text="game?.cost/1000 + 'k'" />
         <td class="num qty" v-text="game?.qty" :title="`${game?.qty} @ ${dayjs.unix(game?.time).format()}`" />
         <td class="num cache" v-text="game?.qty == game?.api ? '-' : game?.redeems?.length" :title="game?.redeems?.length + 'logged'" />
         <td class="num api" v-text="game?.qty == game?.api ? '-' : game?.api" :title="`current SE quantity` "/>
         <td class="num calc" :class="(game?.qty - (game?.redeems?.length??0)) == game?.api ? 'good':'else'">
           <CheckboxMarkedCircleOutline v-if="(game?.qty - (game?.redeems?.length??0)) == game?.api" />
-          <span v-else>{{  (game?.qty - game?.redeems?.length) - game?.api  }}?</span>
+          <span v-else-if="game?.enabled">{{  (game?.qty - game?.redeems?.length) - game?.api  }}?</span>
         </td>
         <td class=redeemRow>
-          <details v-if="game?.redeems?.length" :open="mismatched">
+          <details v-if="game?.redeems?.length" :open="mismatched || findUser?.length">
             <summary>{{  game?.redeems?.length }} redeems</summary>
             <ul class="redeem">
               <template v-for="(u,ux) of game?.redeems" :key="ux">
-                <ul>
+                <ul :class="{thisUser: findUser?.length && u?.user?.toLowerCase()?.includes( findUser?.toLocaleLowerCase() )}">
                   <li class="date font-mono" v-text="dayjs.unix(u.time/1000).format('MMM Do HH:mm')" />
                   <li class="font-bold" v-text="u.user" />
                 </ul>
@@ -103,7 +130,8 @@ const snapshot = computed(()=>{
 <style scoped>
 section { @apply w-full pb-10 }
 article, table { @apply mx-auto py-2 }
-td { @apply px-1 }
+article { @apply items-center }
+td { @apply px-1 align-top }
 tr { @apply even:bg-purple-950 even:text-purple-400 border-b border-purple-950 }
 .num { @apply text-right font-mono w-12 }
 .name { @apply font-bold }
@@ -119,4 +147,6 @@ tr:has(.else) { @apply text-red-500 }
 .redeemRow { @apply text-sm }
 input { @apply bg-transparent p-0 m-0 border-0 w-max }
 label { @apply ring-1 }
+label.on { @apply font-bold bg-purple-950 text-purple-200 }
+.thisUser { @apply text-pink-400 }
 </style>
