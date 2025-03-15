cf523a9aa7e82cfd66d797cb282b769abf513ac0
marc bulbulian <boolioncube@gmail.com>
2025-03-15 15:39:19 -0400

    iFrame init
    - new page hides central components (in app.vue)
    - load userEngagment w/ thisUser from url

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <script setup>
 const { elements, savedData } = inject('$getters')
+const dataStore = inject('$dataStore')
 elements.ls() // init data
 // init: persistance f/ browserStorage
 savedData('ignored')
 savedData('have') 
 savedData('basket')
+const miniModay = computed(() => dataStore?.mini )
 </script>
 
 <template>
   <div class="relative">
-    <TopBar class="sticky top-0 z-10 text-white mb-2" />
-    <ClamsHaveNoValue />
-    <WeeklyData />
-    <MyBasket />
+    <template v-if="!miniModay">
+      <TopBar class="sticky top-0 z-10 text-white mb-2" />
+      <ClamsHaveNoValue />
+      <WeeklyData />
+      <MyBasket />
+    </template>
     <main>
       <router-view />
     </main>
---
+++ src/pages/frame/[user].vue
@@ -0,0 +1,11 @@
+<script setup>
+const route = useRoute()
+const dataStore = inject('$dataStore')
+const user = ref(route?.params?.user?.toLowerCase())
+dataStore.mini = true
+</script>
+<template>
+  <div>
+    <UserEngagment :user="user" />
+  </div>
+</template>
